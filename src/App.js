9bdc68b9b7037d7211b31b16cb10211fb73c6396
skystar234 <80614306+skystar234@users.noreply.github.com>
2021-08-07 22:23:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 /* esLint-disable*/
 
+import './SomeForm.js';
 import './App.css';
 import React, {useState} from 'react';
+import {Helmet} from 'react-helmet';
 
-
-function App() {
+function App1() {
   
   let [따봉, 따봉변경] = useState(0);
 
   let [ 글제목,글제목변경] = useState(['남자 코트 추천', '하루만에 끝내자','블로그 마지막 리스트']);
   let [날짜, 날짜변경] = useState(['2021-08-03', '2021-08-01', '2021-08-02']);
+
+  function ListItem(props){
+    const value = props.value;
+    return(
+      <li key = {value.toString()}>
+        {value}
+      </li>
+    );
+  }
+
+  function NumberList(props){
+    const numbers = props.numbers;
+    const listItems = numbers.map((number)=>
+    <ListItem value ={number}/>
+    );
+    return(
+      <ul>
+        {listItems}
+      </ul>
+    );
+  }
+  
+  
 
   function 제목바꾸기(){
 
@@ -20,16 +44,48 @@ function App() {
   }
 
   function 최신순(){
-    let newArray1 =[...글제목];
-    let newArray2 = [...날짜];
+    let mapped = 날짜.map(function(el, i){
+      return {index: 글제목[i], value : el};
+    })
+    mapped.sort(function(a,b){
+      return +(a.value>b.value)||+(a.value===b.value)-1;
+    });
 
-    newArray1.map(newArray2.sort());
+    let time =[]
+    let title = []
 
-    글제목변경(newArray1);
-    날짜변경(newArray2);
+    for(let i =0;i<mapped.length;i++){
+      title.push(mapped[i]['index']);
+      time.push(mapped[i]['value']);
+    }
+
+    글제목변경(title);
+    날짜변경(time);
+  }
+
+  function 오래된순(){
+    let mapped = 날짜.map(function(el, i){
+      return {index: 글제목[i], value : el};
+    })
+    mapped.sort(function(a,b){
+      return -(a.value>b.value)||-(a.value===b.value)-1;
+    });
+
+    let time =[]
+    let title = []
+
+    for(let i =0;i<mapped.length;i++){
+      title.push(mapped[i]['index']);
+      time.push(mapped[i]['value']);
+    }
+
+    글제목변경(title);
+    날짜변경(time);
   }
 
   return (
+
+    
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>       
@@ -39,21 +95,73 @@ function App() {
         
       </div>
       <div style = {{padding : "20px"}}>
-        <button style = {{color : 'black', backgroundColor:'white', display:'flex', padding:'10px'}} onClick={최신순}><strong>오래된 순</strong></button>
+        <button style = {{color : 'black', backgroundColor:'white', display:'flex', padding:'10px'}} onClick={오래된순}><strong>오래된 순</strong></button>
       </div>
+
+      
       <div className = "list">
-        <h3>{글제목[0]} <button onClick={()=>{따봉변경(따봉+1)}}>👍</button>{따봉}</h3>
-        <p>{날짜[0]}</p>
+        <PostList 
+          value = {0}
+          title = {글제목}
+          date = {날짜}
+          따봉 = {따봉}
+          따봉변경= {따봉변경}
+          />
         <hr/>
-        <h3>{글제목[1]}</h3>
-        <p> {날짜[1]}</p>
+        <PostList
+          value = {1}
+          title = {글제목}
+          date = {날짜}
+          따봉={따봉}
+          따봉변경={따봉변경}
+          />
         <hr/>
-        <h3>{글제목[2]}</h3>
-        <p>{날짜[2]}</p>
+        <PostList
+          value = {2}
+          title = {글제목}
+          date = {날짜}
+          따봉={따봉}
+          따봉변경={따봉변경}
+          />
         <hr></hr>
       </div>
+      
+
+      <Modal></Modal>
     </div>
   );
 }
 
-export default App;
+function PostList({value, title,date, 따봉, 따봉변경}){
+  if(value ===0){
+    return (
+      <div>
+        <h3>{title[value]}<button onClick = {()=>{따봉변경(따봉+1)}}>👍</button>{따봉}</h3>
+        <p>{date[value]}</p>
+      </div>
+    )
+  }
+  else{
+    return(
+      <div>
+        <h3>{title[value]}</h3>
+        <p> {date[value]}</p>
+      </div>
+    )
+  }
+}
+
+function Modal(){// component 명 대문자 시작
+  return (
+    <>
+    <div className = "modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    <div></div>
+    </>
+  )
+}
+
+export default App1;
